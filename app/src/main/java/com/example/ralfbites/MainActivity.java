7896c34c5d021a537a769e1f575fca5d88a56d38
niccm <nicomederos7@gmail.com>
2023-04-18 13:34:18 -0700

    New Branc

--- app/src/main/java/com/example/ralfbites/MainActivity.java
+++ app/src/main/java/com/example/ralfbites/MainActivity.java
@@ -8,6 +8,11 @@ import android.os.Bundle;
  * @author Nicolas Mederos
  * @since 18 April 2023
  * Starting the project
+ * Make a change
+ * Then go git
+ * Press plus sign for a NEW branch
+ * Create the new branch
+ * Make sure ur in the right branch when commiting
  */
 
 public class MainActivity extends AppCompatActivity {
